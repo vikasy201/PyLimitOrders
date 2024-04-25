e057dab84f9350be62e5b9856b7da83926135ff5
vikas yadav <vikasy201@gmail.com>
2024-04-26 01:19:57 +0530

    correct code in on_price_tick

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -1,6 +1,6 @@
 from trading_framework.execution_client import ExecutionClient
 from trading_framework.price_listener import PriceListener
-
+import requests
 
 class LimitOrderAgent(PriceListener):
 
@@ -11,7 +11,7 @@ class LimitOrderAgent(PriceListener):
         """
         super().__init__()
         self.execution_client = execution_client
-        self.orders: list[tuple[str, int, float]] = []
+        self.orders: list[tuple[str, int, float, bool]] = []
 
     def add_order(self, buy: bool, product_id: str, amount: int, limit_price: float):
         self.orders.append((product_id, amount, limit_price, buy))
@@ -32,7 +32,11 @@ class LimitOrderAgent(PriceListener):
             self.orders.remove(order)
 
 
-    def on_price_tick(self, product_id: str, price: float):
+    def on_price_tick(self, product_id: str):
         # see PriceListener protocol and readme file
-        if product_id == "IBM" and price < 100:
-            self.execution_client.buy("IBM", 1000, price)
+        # Here we need to put the link to fetch the Price for a Particular Product ID
+        api_link = "some_api_link/" + product_id
+        response = requests.get(api_link)
+        return response.json()
+        # if product_id == "IBM" and price < 100:
+        #     self.execution_client.buy("IBM", 1000, price)
